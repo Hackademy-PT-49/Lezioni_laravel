a5dbc88f854eb5f77d1a6786d97483f2e2128cee
Emanuele Trossarello <emanuele.trossarello@aulab.it>
2025-02-14 21:43:01 +0100

    Lezione 16 con fetch corretta

--- Lezione16-API/AnimeAPI/resources/views/welcome.blade.php
+++ Lezione16-API/AnimeAPI/resources/views/welcome.blade.php
@@ -4,5 +4,15 @@
 
         </ul>
     </div>
+
+    <script> const list = document.getElementById('item-list')
+        fetch('/api/v1/test').then(response=>response.json()).then(elements=>{
+            for(let item of elements.data){
+                list.innerHTML += `<li>${item.title}</li>`
+            }
+        })
+    
+    
+    </script>
     
 </x-layout>
